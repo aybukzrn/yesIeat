97a50a2aa71a6c38ec5c71db4dd225aae64fd7ac
Selinay Türksal <selinayturksal@gmail.com>
2025-10-19 13:18:54 +0300

    kaydettim

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 // Sayfaları içe aktarın
+import Login from './pages/LoginPage';
+import Register from './pages/RegisterPage';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
 import Account from './pages/Account';
@@ -11,6 +13,8 @@ function App() {
   return (
     
     <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/home" element={<Home />} />
       <Route path="/menu" element={<Menu />} />
       <Route path="/account" element={<Account />} />
