0f6905574051c6d39c2a6c60cea63b719d2f037c
aybukzrn <aybukezrn@gmail.com>
2025-10-27 00:56:00 +0300

    Kayıtlı Adreslerim sayfasına yeni adres ekleme formu eklendi; CSS stilleri oluşturuldu ve güncellendi.

--- src/pages/Legal2/KayitliAdreslerimContent.jsx
+++ src/pages/Legal2/KayitliAdreslerimContent.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import './KayitliAdreslerimContent.css'
+import { FaMapMarkerAlt } from "react-icons/fa";
+
 
 const KayitliAdreslerimContent = () => {
   return (
-    <div>
-      
+    <div className="addresses">
+      <div className="header">
+        <h3>Kayıtlı Kartlarım</h3>
+
+        <div className="add-card">
+          <button>
+            <FaMapMarkerAlt className='add-address'/>Yeni Adres Ekle
+          </button>
+        </div>
+      </div>
+
+      <div className="address-form">
+        <div className="title">
+          <input type="text" 
+          placeholder='Adres Başlığı'/>
+        </div>
+
+        <div className="triple">
+          <div className="building">
+            <input type="number" 
+            placeholder='Bina No'/>
+          </div>
+          <div className="floor">
+            <input type="number"
+            placeholder='Kat No'/>
+          </div>
+          <div className="apartment">
+            <input type="number"
+            placeholder='Daire No'/>
+          </div>
+        </div>
+
+        <div className="addess">
+          <input type="text" 
+          placeholder='Açık Adres'/>
+        </div>
+
+        <div className="phone">
+          <input type="tel"
+          placeholder='Cep Telefonu'/>
+        </div>
+      </div>
     </div>
   )
 }
